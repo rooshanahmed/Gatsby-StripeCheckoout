59977d68cd0f0581c291478b560ebd480e708638
Rooshan Ahmed <rooshan.ahmed@hotmail.com>
2020-12-29 01:35:47 +0500

    code updated

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,12 +1,8 @@
 import React from "react"
-import { loadStripe } from "@stripe/stripe-js"
 import { graphql, useStaticQuery } from "gatsby"
 
-const stripePromise = loadStripe(
-  "pk_test_51I3GslEnWioBjOItEgqTwSFPgdBn1Nsf1bA7wx3nOSH5ww1GDvNl3hipOrMc9ZAUbDVhFAd0HJ7WBk4JF59rJecn00lvgFgrUC"
-)
 
-export default function Home({ location }) {
+export default function Home() {
   const data = useStaticQuery(
     graphql`
       query ProductPrices {
